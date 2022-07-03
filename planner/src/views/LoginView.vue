0a658cf4849946a80b1741cc1d6bfdf63811c4e3
Carlos Muñoz <carmuny54@gmail.com>
2022-07-03 20:15:24 +0200

    Login screen

--- planner/src/views/LoginView.vue
+++ planner/src/views/LoginView.vue
@@ -1,8 +1,97 @@
 <template>
 
-    <div class="login">
+    <div class="login" >
 
+        <v-row>
 
+            <v-col cols="12" md="6" sm="12" class="primary py-5">
+                <v-container class="fotm-container">
+                    <v-form v-model="valid"  >
+                        <v-row>
+                            <v-col
+                            cols="12"
+                            >   
+                                <div class="d-flex justify-center">
+                                    <v-img
+                                        alt="Vuetify Logo"
+                                        class="shrink mr-2"
+                                        contain
+                                        src="/img/Logos/Logo.svg"
+                                        transition="scale-transition"
+                                        width="70"
+                                    />
+                                    <v-img
+                                        alt="Vuetify Name"
+                                        class="shrink mt-1 hidden-sm-and-down"
+                                        contain
+                                        min-width="100"
+                                        src="/img/Logos/logoText.png"
+                                        width="190"
+                                    />
+                                </div>
+                            <v-col
+                            cols="12"
+                            class="text-center"
+                            
+                            >   
+                                <span class="white--text login-title">Bienvenido de Vuelta!</span>
+                            </v-col>
+                            </v-col>
+                            <v-col
+                            cols="12"
+                            >
+                            <v-text-field
+                                v-model="firstname"
+                                :rules="nameRules"
+                                :counter="10"
+                                label="First name"
+                                dark
+                            ></v-text-field>
+                            </v-col>
+
+                            <v-col
+                            cols="12"
+                            >
+                                <v-text-field
+                                    v-model="lastname"
+                                    :rules="nameRules"
+                                    :counter="10"
+                                    label="Last name"
+                                    dark
+                                ></v-text-field>
+                            </v-col>
+
+                            <v-col
+                            cols="12"
+                            >
+                                <v-row align="center">
+                                    <v-col cols="6">
+                                        <v-checkbox
+                                            v-model="keep_login"
+                                            :label="`Keep me logged in`"
+                                            dark
+                                        ></v-checkbox>
+                                    </v-col>
+                                    <v-col cols="6" class="text-right">
+                                        <v-btn text dark left href="/fooRoute" target="_blank">Forget your password?</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-col>
+
+                            <v-col
+                            cols="12"
+                            >
+                            <v-btn block color="white"><span class="primary--text">Login</span></v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-form>
+                </v-container>
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+
+            </v-col>
+
+        </v-row>
 
     </div>
 
@@ -11,9 +100,40 @@
 <script>
 
   export default {
+    data: () => ({
+        valid: false,
+        firstname: '',
+        lastname: '',
+        keep_login:false,
+        nameRules: [
+            v => !!v || 'Name is required',
+            v => v.length <= 10 || 'Name must be less than 10 characters',
+        ],
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+/.test(v) || 'E-mail must be valid',
+        ],
+    }),
     metaInfo: {
         title: 'Login',
     },
   }
 
 </script>
+
+<style lang="scss">
+
+
+
+    .fotm-container{
+        max-width: 500px;
+        min-height: calc(100vh - 116px);
+        display: flex;
+        align-items: center;
+        .login-title{
+            font-size: 36px;
+            font-weight: 500;
+        }
+    }
+
+</style>
--- server/app.js
+++ server/app.js
@@ -5,9 +5,10 @@ const port = 3000;
 app.use(express.json());
 app.use(
     express.urlencoded({
-      extended: true,
+        extended: true,
     })
-  );
+);
+app.use(express.urlencoded({ extended: true }));
 
 var connection = mysql.createConnection({
   host     : '192.168.1.60',
@@ -29,25 +30,36 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
 
-    var monka = {
-        data:[],
-        message:"ok"
-    };
-    connection.connect();
+    if(!req.body.username && !req.body.password) {
+        res.json({message:"Bad parameters",status:"error"});
+        return;
+    }else if(req.body.username=='' || req.body.password==''){
+        res.json({message:"Bad parameters",status:"error"});
+        return;
+    }
 
-    connection.query('SELECT * FROM users', function(err, rows, fields) {
+    username = req.body.username;
+    password = req.body.password;
+
+
+    
+
+    connection.query("SELECT * FROM users where name='"+username+"' and password='"+password+"' limit 1", function(err, rows, fields) {
         if (err) throw err;
+        console.log(rows.length);
 
-        for (let i = 0; i < rows.length; i++) {
-            const user = rows[i];
-            monka.data.push(user.name)
+        if(rows.length===1){
+            res.json({status:'ok',message:'Login Correcto',token:''});
+        }else{
+            res.json({status:'error',message:'Login incorrecto'});
         }
         
-        res.json(monka);
 
     });
-    connection.end();
+
+    
 });
+
 
 
 
